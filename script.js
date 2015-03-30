aea9e905a5b6f09269c00203df1fccfcbb1be694
Jongjai Lochman <jongfreedom@gmail.com>
2015-03-31 00:33:33 +0100

    add div class total, in order to add task counter.

--- script.js
+++ script.js
@@ -7,11 +7,20 @@ $(document).on('click', '#add-to-list', function addToList(event){
 	$("input#item").focus();
 	
 });*/
+function updateTotal(){
+	//$(".total").text("Pending: ");
+	var pending = $(".pending").length;
+	var completed = $(".success").length;
+
+	$(".total").text("Pending: " + pending + " complete: " + completed);
+}
+
 $(document).on('click', '#add-to-list', function addToList(event){
 	var item = $("input").val();
 	$("ol#items").append("<li>" + item + "<span class='label pending'>Pending</span></li>");
 	$("input#item").val("");
 	$("input#item").focus();
+	updateTotal();
 	
 });
 
@@ -19,7 +28,10 @@ $(document).on('click', '.pending', function addNewClass(){
 	$(this).parent().addClass("completed");
 	$(this).parent().append("<span class='label success'>Done!</span>");
 	$(this).remove();
+	updateTotal();
 });
+
+
 
 
 
